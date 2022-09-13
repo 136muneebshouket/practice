00eb9f5ccf68669a3aacbdb0edc61be8819d3ee7
136muneebshouket <92539069+136muneebshouket@users.noreply.github.com>
2022-09-13 05:02:40 -0700

    practice

--- practice.js
+++ practice.js
@@ -16,32 +16,46 @@
 
 
 // Pretend that this response is coming from the server
-const students = [
-    {name: "harry", subject: "JavaScript"},
-    {name: "Rohan", subject: "Machine Learning"}
-]
+// const students = [
+//     {name: "harry", subject: "JavaScript"},
+//     {name: "Rohan", subject: "Machine Learning"}
+// ]
 
 
-function enrollStudent(student, callback){
-    setTimeout(function() {
-        students.push(student);
-        console.log("Student has been enrolled");
-        callback();
-    }, 1000);
-}
+// function enrollStudent(student, callback){
+//     setTimeout(function() {
+//         students.push(student);
+//         console.log("Student has been enrolled");
+//         callback();
+//     }, 1000);
+// }
 
-function getStudents(){
-    setTimeout(function() {
-        let str = "";
-        students.forEach(function(student){
-            str += `<li> ${student.name}  ""${student.subject}</li>`
-        });
-        document.getElementById('students').innerHTML = str;
-        console.log("Students have been fetched");
-    }, 5000);
-}
+// function getStudents(){
+//     setTimeout(function() {
+//         let str = "";
+//         students.forEach(function(student){
+//             str += `<li> ${student.name}  ""${student.subject}</li>`
+//         });
+//         document.getElementById('students').innerHTML = str;
+//         console.log("Students have been fetched");
+//     }, 5000);
+// }
 
-let newStudent = {name:"Sunny", subject:"Python"}
-enrollStudent(newStudent, getStudents);
+// let newStudent = {name:"Sunny", subject:"Python"}
+// enrollStudent(newStudent, getStudents);
 // getStudents();
+
+
+
+class programer extends employee{
+constructor(givenname, givenexperience, givendivision,language,github){
+super(givenname, givenexperience, givendivision);
+this.language = language;
+this.github = github;
+}
+
+}
+
+ let employe = new programer("mirza",0,"javascript","javascript","muneeb136");
+ console.log(employe); 
 
