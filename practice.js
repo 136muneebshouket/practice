21a42c4116518066bf80e2fa663ff818056335f9
136muneebshouket <92539069+136muneebshouket@users.noreply.github.com>
2022-09-14 04:43:44 -0700

    2022-09-14T04:43:44-07:00

--- practice.js
+++ practice.js
@@ -1,13 +1,47 @@
-var arr1=[6,4,8,10,9];
-var arr2=[5,10,8,9,4];
+// function addition(x, y , callback){
+//     setTimeout(() => {
+//     document.write(`The sum of ${x} and ${y} is ${x+y}.`); 
+//     callback(); 
+//      }, 5000); } 
+//      // display() function is called just after the ending of addition() function
+//     function mycallback(){ 
+//     document.write('Numbers added and thank u'); 
+//      } 
+//      // Calling addition() function
+//     addition(5,5,mycallback);
 
-const issame=
-arr1.length == arr2.length &&
-arr1.every((currelement)=>{
-     if(arr2.indexOf(currelement)>-1){
 
-        return (currelement = arr2[arr2.indexOf(currelement)])
-     }
- 
-})
-console.log(issame);
+
+
+
+
+// Pretend that this response is coming from the server
+const students = [
+    {name: "harry", subject: "JavaScript"},
+    {name: "Rohan", subject: "Machine Learning"}
+]
+
+
+function enrollStudent(student, callback){
+    setTimeout(function() {
+        students.push(student);
+        console.log("Student has been enrolled");
+        callback();
+    }, 1000);
+}
+
+function getStudents(){
+    setTimeout(function() {
+        let str = "";
+        students.forEach(function(student){
+            str += `<li> ${student.name}  ""${student.subject}</li>`
+        });
+        document.getElementById('students').innerHTML = str;
+        console.log("Students have been fetched");
+    }, 5000);
+}
+
+let newStudent = {name:"Sunny", subject:"Python"}
+enrollStudent(newStudent, getStudents);
+// getStudents();
+
