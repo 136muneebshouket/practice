9c67817cc5d19b495c6dbd18e0862338e292d5f8
136muneebshouket <92539069+136muneebshouket@users.noreply.github.com>
2022-10-14 06:07:57 +0500

    factorial of anumber

--- practice.js
+++ practice.js
@@ -1671,18 +1671,29 @@
 
 
 // on array
-[a, b, c, ...d] = [1,2,3,4,5,6,7,8,9,10, 11, 12, 13];
-console.log(a)
-console.log(b)
-console.log(c)
-console.log(d)
+// [a, b, c, ...d] = [1,2,3,4,5,6,7,8,9,10, 11, 12, 13];
+// console.log(a)
+// console.log(b)
+// console.log(c)
+// console.log(d)
 
 
 
-({a, b, c, ...d} = {a: 34, b:345, c:67, d:45, e:34})
-console.log(a, b, c, d)
+// ({a, b, c, ...d} = {a: 34, b:345, c:67, d:45, e:34})
+// console.log(a, b, c, d)
 
 
-const fruits = ['Apple', 'Bananas', 'Mangoes'];
-[a, b, c] = fruits;
-console.log(a, b, c);
+// const fruits = ['Apple', 'Bananas', 'Mangoes'];
+// [a, b, c] = fruits;
+// console.log(a, b, c);
+
+
+// finding factorial f a number//
+
+let num=prompt("give number")
+var fact=1;
+for(var i=1; i<=num ;i++){
+   fact=fact*i;
+ 
+}
+console.log(fact)
